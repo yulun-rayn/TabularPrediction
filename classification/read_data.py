8c8c02bd7068af22c50324713648fc2d281ba092
bqk928 <doron.bergman@capitalone.com>
2025-01-15 19:53:09 +0000

    include check that 72 datasets are present for benchmarking in relevant split

--- classification/read_data.py
+++ classification/read_data.py
@@ -7,6 +7,8 @@ def get_datasets(split):
     filenames = os.listdir(data_dir)
     filenames = [f for f in filenames if f.endswith(".pt") and (f not in exclusions)]
     
+    assert len(filenames) == 68, f"ALERT! missing some *.pt files - there should be 72 in total - 68 for evaluation plus 4 exclusions. Found only {len(filenames)}"
+    
     # Create a list of tuples containing file name and size
     file_sizes = [(f, os.path.getsize(os.path.join(data_dir, f))) for f in filenames]
 
